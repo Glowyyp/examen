c91ecd2c55e52664b975f3e4b8a706289648750e
luis <lu.lizanaq@duocuc.cl>
2024-07-12 14:17:36 -0400

    cambios

--- bibliotastic/catalogo/urls.py
+++ bibliotastic/catalogo/urls.py
@@ -6,4 +6,10 @@ urlpatterns = [
     path('libros/', views.libros, name='libros'),
     path('autores/', views.autores, name='autores'),
     path('categorias/', views.categorias, name='categorias'),
+    path('buscar_libros/', views.buscar_libros, name='buscar_libros'),
+    path('obtener_libro/<int:libro_id>/', views.obtener_libro, name='obtener_libro'),
+    
+    
+    
+    
 ]
--- bibliotastic/catalogo/views.py
+++ bibliotastic/catalogo/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
-from .models import Libro, Autor, Categoria
+from django.shortcuts import get_object_or_404
+from .models import Libro, Autor, Categoria, NavItem
+from django.http import JsonResponse
+
+
 
 # Create your views here.
 
@@ -17,3 +21,40 @@ def autores(request):
 def categorias(request):
     categorias = Categoria.objects.all()
     return render(request, 'catalogo/categorias.html', {'categorias': categorias})
+
+def buscar_libros(request):
+    if request.method == 'GET' and 'titulo' in request.GET:
+        titulo_libro = request.GET.get('titulo', '')
+        libros = Libro.objects.filter(titulo__icontains=titulo_libro)
+
+      
+        libros_json = []
+        for libro in libros:
+            libros_json.append({
+                'titulo': libro.titulo,
+                'anio_publicacion': libro.anio_publicacion,
+                'descripcion_breve': libro.descripcion_breve
+            })
+
+       
+        return JsonResponse({'libros': libros_json})
+
+    
+    return JsonResponse({'libros': []})
+
+
+
+
+
+def obtener_libro(request, libro_id):
+    libro = get_object_or_404(Libro, id=libro_id)
+    data = {
+        'titulo': libro.titulo,
+        'anio_publicacion': libro.anio_publicacion,
+        'descripcion_breve': libro.descripcion_breve,
+        # Agregar más campos según sea necesario
+    }
+    return JsonResponse(data)
+
+
+
